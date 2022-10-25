662d59e0f61ac9a6770c91c17fc83bf7d84a7160
Gideon Anokye <gideonanokyebrown@gmail.com>
2022-10-25 12:06:54 +0000

    modified changes

---
+++ src/Calc.js
@@ -0,0 +1,42 @@
+import './Calc.css';
+import { useState } from 'react';
+
+const Calc = () => {
+  const [result, setResult]= useState("");
+  const clickHandle =(event)=>{
+    setResult(result.concat(event.target.value));
+  }
+    const clearDisplay = ()=>{
+      setResult("")
+    }
+    const calculate = ()=>{
+      setResult(eval(result).toString())
+    }
+  return (
+    <div className='calc'>
+       <input type='text' placeholder='0' id='answer' value={result}/>
+       <input type='button' value={'9'} className="button" onClick={clickHandle}/>
+       <input type='button' value={'8'} className="button" onClick={clickHandle} />
+       <input type='button' value={'7'} className="button" onClick={clickHandle}/>
+       <input type='button' value={'6'} className="button" onClick={clickHandle}/>
+       <input type='button' value={'5'} className="button" onClick={clickHandle}/>
+       <input type='button' value={'4'} className="button" onClick={clickHandle}/>
+       <input type='button' value={'3'} className="button" onClick={clickHandle}/>
+       <input type='button' value={'2'} className="button" onClick={clickHandle}/>
+       <input type='button' value={'1'} className="button" onClick={clickHandle}/>
+       <input type='button' value={'0'} className="button" onClick={clickHandle}/>
+       <input type='button' value={'.'} className="button" onClick={clickHandle}/>
+       <input type='button' value={'+'} className="button" onClick={clickHandle}/>
+       <input type='button' value={'-'} className="button" onClick={clickHandle}/>
+       <input type='button' value={'*'} className="button" onClick={clickHandle}/>
+       <input type='button' value={'/'} className="button" onClick={clickHandle}/>
+       <input type='button' value={'%'} className="button" onClick={clickHandle}/>
+       <input type='button' value={'Del'} className="button button1" onClick={clearDisplay} />
+       <input type='button' value={'='} className="button button1" onClick={calculate}/>
+
+       
+    </div>
+  )
+}
+
+export default Calc
